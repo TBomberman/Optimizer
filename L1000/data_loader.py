dd89dbea0135c8c851a1db3278c1ae7e12f34e55
llamosa <michael.llamosa@gmail.com>
2018-01-29 13:50:32 -0500

    -Testing RF in x.csv and y.csv

--- L1000/data_loader.py
+++ L1000/data_loader.py
@@ -52,7 +52,7 @@ def get_feature_dict(file, delimiter=',', key_index=0):
         next(reader)
         return dict((row[key_index], row[1:]) for row in reader)
 
-def load_gene_expression_data(lm_gene_entrez_ids):
+def load_gene_expression_data(file,lm_gene_entrez_ids):
     return parse(
-        "/home/gwoo/Data/L1000/LDS-1191/Data/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx",
+        file,
         col_meta_only=False, row_meta_only=False, rid=lm_gene_entrez_ids)
