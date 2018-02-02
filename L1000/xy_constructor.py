f5f0a4e9a25ae6ac7315136a2fb7ea08fb8f9b62
Godwin <godwinwoo@hotmail.com>
2018-02-02 10:53:57 -0800

    Evaluate per cell line.

--- L1000/xy_constructor.py
+++ L1000/xy_constructor.py
@@ -1,7 +1,8 @@
-from L1000.data_loader import get_feature_dict, load_gene_expression_data
+from L1000.data_loader import get_feature_dict, load_gene_expression_data, printProgressBar
 import json
 from random_forest import do_optimize
 import numpy as np
+import gc
 
 def find_nth(haystack, needle, n):
     start = haystack.find(needle)
@@ -19,21 +20,32 @@ def get_gene_id_dict():
 
 # get the dictionaries
 # get the expressions
+print("Loading drug and gene features")
 drug_features_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/WorkingData/smiles_rdkit_maccs.csv', use_int=True)
 cell_features_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/WorkingData/cell_line_fingerprint.csv', use_int=True)
 gene_features_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/WorkingData/gene_go_fingerprint.csv', use_int=True)
 batch_to_drug_id_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/Metadata/Small_Molecule_Metadata.txt', '\t', 9)
 cell_name_to_id_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/Metadata/Cell_Line_Metadata.txt', '\t', 0)
 gene_id_dict = get_gene_id_dict()
-lm_gene_entrez_ids = list(gene_id_dict.keys())
+lm_gene_entrez_ids = list(gene_id_dict.keys())[:50]
+print("Loading gene expressions from gctx")
 level_5_gctoo = load_gene_expression_data("/home/gwoo/Data/L1000/LDS-1191/Data/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx", lm_gene_entrez_ids)
 
 length = len(level_5_gctoo.col_metadata_df.index)
-X = []
-Y = []
+
+cell_X = {}
+cell_Y = {}
+cell_drugs = {}
 
 # For every experiment
+print("Loading experiments")
+one_percent = int(length/50)
 for i in range(length):
+    if i % one_percent == 0:
+        printProgressBar(i, length, prefix='Progress:', suffix='Complete', length=50)
+    X = []
+    Y = []
+
     col_name = level_5_gctoo.col_metadata_df.index[i]
     column = level_5_gctoo.data_df[col_name]
 
@@ -63,13 +75,41 @@ for i in range(length):
 
         if gene_symbol not in gene_features_dict:
             continue
-        X.append(drug_features + cell_features + gene_features_dict[gene_symbol])
-        Y.append(column[gene_id])
 
-npX = np.asarray(X)
-npY = np.asarray(Y)
-npY_class = np.zeros(len(npY), dtype=int)
-npY_class[np.where(npY>0)] = 1
-# np.savetxt("x.csv", npX, fmt='%i')
-# np.savetxt("y.csv", npY_class, fmt='%i')
-do_optimize(0, npX, npY_class)
+        if cell_id not in cell_X:
+            cell_X[cell_id] = []
+            cell_Y[cell_id] = []
+            cell_drugs[cell_id] = []
+
+        cell_X[cell_id].append(drug_features + gene_features_dict[gene_symbol])
+        cell_Y[cell_id].append(column[gene_id])
+        cell_drugs[cell_id].append(drug_id)
+
+gc.collect()
+count = 1
+for cell_name in cell_name_to_id_dict:
+    cell_id = cell_name_to_id_dict[cell_name][0]
+    if cell_id not in cell_X:
+        print("Skipping", cell_name, ". No cell line data.\n")
+        continue
+
+    npX = np.asarray(cell_X[cell_id])
+    npY = np.asarray(cell_Y[cell_id])
+
+    sample_size = len(npX)
+
+    if sample_size < 3:
+        print("Skipping", cell_name, ". Sample size", sample_size, "is too small.\n")
+        continue
+
+    npY_class = np.zeros(len(npY), dtype=int)
+    npY_class[np.where(npY > 0)] = 1
+
+    # np.savetxt("x.csv", npX, fmt='%i')
+    # np.savetxt("y.csv", npY_class, fmt='%i')
+
+    print("Evaluating cell line", count, cell_name)
+    print("Sample Size:", sample_size, "Drugs tested:", len(set(cell_drugs[cell_id])))
+    do_optimize(0, npX, npY_class)
+    count += 1
+    print()
--- random_forest.py
+++ random_forest.py
@@ -45,25 +45,31 @@ def do_optimize(nb_classes, data, labels, data_test=None, labels_test=None):
     Y_val = y_val
 
     # model = RandomForestRegressor()
+    total_features = len(data[0])
+    # for i in range(1,10):
+    i = 1
+    # num_features = int((total_features*i/10)**(1/2))
+    min_samples_split = 2**i
+    estimators = 10 * i
+    max_depth = 5 * i
     model = RandomForestClassifier()
 
     model.fit(X_train, Y_train)
     y_pred = model.predict(X_train)
     # tr_error = np.mean((Y_train - y_pred)**2)
     tr_error = np.mean(y_pred != Y_train)
-    print("Random Forest (sklearn) train error: %.3f" % tr_error)
     # scatter2D_plot(y_train, y_pred, "train", "rf")
 
     y_pred = model.predict(X_test)
     # te_error = np.mean((Y_test - y_pred) ** 2)
     te_error = np.mean(y_pred != Y_test)
-    print("Random Forest (sklearn) test error: %.3f" % te_error)
     # scatter2D_plot(Y_test, y_pred, "test", "rf")
 
     y_pred = model.predict(X_val)
     # v_error = np.mean((Y_val - y_pred) ** 2)
     v_error = np.mean(y_pred != Y_val)
-    print("Random Forest (sklearn) validation error: %.3f" % v_error)
     # scatter2D_plot(Y_val, y_pred, "val", "rf")
 
+    avg_test = (te_error + v_error)/2
+    print("Random Forest (sklearn) error train: %.3f test: %.3f val: %.3f avg test: %.3f" % (tr_error, te_error, v_error, avg_test))
 
