0d8b38425baaed53a88eee6e669ac710160f6666
Godwin <godwinwoo@hotmail.com>
2018-04-09 12:59:32 -0700

    -Increase printout precision

--- helpers/callbacks.py
+++ helpers/callbacks.py
@@ -11,4 +11,4 @@ class NEpochLogger(Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         if epoch % self.display == 0:
-            print('{0}: {1}/{2} - Epoch Train Loss: {3:6.3f} Val Loss: {4:6.3f}'.format(datetime.datetime.now(), epoch, self.params['epochs'], logs['loss'], logs['val_loss']))
+            print('{0}: {1}/{2} - Epoch Train Loss: {3:8.7f} Val Loss: {4:8.7f}'.format(datetime.datetime.now(), epoch, self.params['epochs'], logs['loss'], logs['val_loss']))
