69cb34d46b24a7eb393760d83adca09eee5ee274
Godwin <godwinwoo@hotmail.com>
2018-04-27 14:09:26 -0700

    Stop the top 10 predictions.

--- L1000/get_predictions.py
+++ L1000/get_predictions.py
@@ -2,28 +2,59 @@ from L1000.gene_predictor import Gene_Predictor
 import csv
 import numpy as np
 import helpers.email_notifier as en
+import datetime
+from sortedcontainers import SortedDict
 
-def get_predictions(train_data, train_labels):
+def get_predictions(train_data, train_labels, gene_features_dict={}, lm_gene_entrez_ids=[], gene_id_dict={}):
     model = Gene_Predictor()
     model.train(train_data, train_labels)
 
     file_name = '/data/datasets/gwoo/tox21/ZincCompounds_WaitOK_maccs_full.csv'
-    with open(file_name, "r") as csv_file:
-        reader = csv.reader(csv_file, dialect='excel', delimiter=',')
-        next(reader)
-        counter = 0
-        for row in reader:
-            if counter % 10000 == 0:
-                print("Evaluating molecule #", counter)
-            fingerprint = []
-            for value in row[1:]:
-                fingerprint.append(int(value))
-            molecule_id = row[0]
-            data = np.asarray([fingerprint])
-            prediction = model.predict(data)
-            if prediction[0][0] > prediction[0][1]:
-                print(molecule_id, "Downreglate")
-                en.notify("found compound" + molecule_id)
-            counter += 1
+    top10 = SortedDict()
+    top10_full = False
+    top10_counter = 0
 
+    try:
+        with open(file_name, "r") as csv_file:
+            reader = csv.reader(csv_file, dialect='excel', delimiter=',')
+            next(reader)
+            drug_counter = 0
+            for row in reader:
+                if drug_counter % 10000 == 0:
+                    print(datetime.datetime.now(), "Evaluating molecule #", drug_counter)
+                drug_features = []
+                try:
+                    for value in row[1:]:
+                        drug_features.append(int(value))
+                except:
+                    drug_counter += 1
+                    continue
 
+                for gene_id in lm_gene_entrez_ids:
+                    gene_symbol = gene_id_dict[gene_id]
+                    if gene_symbol not in gene_features_dict:
+                        continue
+                    gene_features = gene_features_dict[gene_symbol]
+
+                    molecule_id = row[0]
+                    data = np.asarray([drug_features + gene_features])
+                    prediction = model.predict(data)
+                    down_probability = prediction[0][0]
+                    if down_probability > 0.5:
+                        message = "Found compound " + str(molecule_id) + " that downregulates " + gene_symbol + " " + str(down_probability)
+                        if top10_full:
+                            if down_probability > top10.keys()[0]:
+                                del top10[top10.keys()[0]]
+                                top10[down_probability] = message
+                                print(message)
+                        else:
+                            top10[down_probability] = message
+                            print(message)
+                            top10_counter += 1
+                            if top10_counter >= 10:
+                                top10_full = True
+
+                drug_counter += 1
+    finally:
+        print(top10)
+        en.notify("Predicting Done")
