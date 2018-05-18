5a7d4e3df2e07cdda9e88994a9a0c2bfb4e00a20
Godwin <godwinwoo@hotmail.com>
2018-05-18 10:36:05 -0700

    Plot ROC tool.

---
+++ helpers/plot_roc.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+def plot_roc(y_true, y_pred):
+    false_positive_rate, true_positive_rate, thresholds = roc_curve(y_true, y_pred)
+    roc_auc = auc(false_positive_rate, true_positive_rate)
+
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([-0.1, 1.2])
+    plt.ylim([-0.1, 1.2])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+
--- random_forest.py
+++ random_forest.py
@@ -5,6 +5,7 @@ from helpers.utilities import scatter2D_plot
 from sklearn.tree import DecisionTreeClassifier # see http://scikit-learn.org/stable/install.html
 from sklearn.metrics import roc_auc_score, roc_curve, auc
 import matplotlib.pyplot as plt
+from helpers.plot_roc import plot_roc
 
 def do_optimize(nb_classes, data, labels, data_test=None, labels_test=None):
     if data_test is None:
@@ -72,15 +73,4 @@ def do_optimize(nb_classes, data, labels, data_test=None, labels_test=None):
         print("Random Forest (sklearn) AUC train: %.3f test: %.3f accuracy: %.3f" % (tr_auc, te_auc, accuracy))
 
         if show_roc:
-            false_positive_rate, true_positive_rate, thresholds = roc_curve(Y_test, y_pred)
-            roc_auc = auc(false_positive_rate, true_positive_rate)
-
-            plt.title('Receiver Operating Characteristic')
-            plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.2f' % roc_auc)
-            plt.legend(loc='lower right')
-            plt.plot([0, 1], [0, 1], 'r--')
-            plt.xlim([-0.1, 1.2])
-            plt.ylim([-0.1, 1.2])
-            plt.ylabel('True Positive Rate')
-            plt.xlabel('False Positive Rate')
-            plt.show()
+            plot_roc(Y_test, y_pred)
