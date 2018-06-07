31ce5c855b6718ca9fe5e19c3fd2c38def94655e
Godwin <godwinwoo@hotmail.com>
2018-06-07 10:57:59 -0700

    Plot histogram of genes.

--- L1000/LDS-1191/plot_gene_histogram.py
+++ L1000/LDS-1191/plot_gene_histogram.py
@@ -1,9 +1,6 @@
-import csv
 import json
-from operator import itemgetter
 
 import matplotlib.pyplot as plt
-import numpy as np
 from L1000.data_loader import load_gene_expression_data
 
 # go through the data set
@@ -12,20 +9,17 @@ from L1000.data_loader import load_gene_expression_data
 
 # load expressions data
 def load_landmark_genes():
+    symbols = {}
     lm_genes = json.load(open('data/landmark_genes.json'))
-    ids = []
     for lm_gene in lm_genes:
-        ids.append(lm_gene['entrez_id'])
-    return ids
+        symbols[lm_gene['entrez_id']] = lm_gene['gene_symbol']
+    return symbols
 
-lm_gene_entrez_ids = load_landmark_genes()
+symbols = load_landmark_genes()
+lm_gene_entrez_ids = ['2778']
 level_5_gctoo = load_gene_expression_data("/home/gwoo/Data/L1000/LDS-1191/Data/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx", lm_gene_entrez_ids)
 
 length = len(level_5_gctoo.col_metadata_df.index)
-
-highest_var = 0
-highest_gene_id = ""
-gene_var = []
 
 # For every gene
 for gene_id in lm_gene_entrez_ids:
@@ -35,23 +29,8 @@ for gene_id in lm_gene_entrez_ids:
         column = level_5_gctoo.data_df[col_name]
         one_gene_expression_values.append(column[gene_id])
 
-    # compute the var
-    variance = np.var(one_gene_expression_values)
-    gene_var.append([gene_id, variance])
-    print(gene_id)
-    print(variance)
-    if variance > highest_var:
-        highest_var = variance
-        highest_gene_id = gene_id
-
-print("Highest:")
-print(highest_gene_id)
-print(highest_var)
-
-gene_var.sort(key=itemgetter(1), reverse=True)
-gene_ids = [tup[0] for tup in gene_var]
-
-with open("data/genes_by_var.csv", "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    for val in gene_ids:
-        writer.writerow([val])
+    plt.title('Histogram of gene: GNAS')
+    plt.ylabel('Number of expressions')
+    plt.xlabel('Z-score')
+    plt.hist(one_gene_expression_values, bins=1000)
+    plt.show()
