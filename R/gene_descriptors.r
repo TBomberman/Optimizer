fe54530491bc3785f0c35b56dc1420a2cc6a468c
gwoo <godwinwoo@hotmail.com>
2018-11-16 17:08:44 -0800

    Save go terms calculation.

---
+++ R/gene_descriptors.r
@@ -0,0 +1,53 @@
+print(setwd("H:/Python/Optimizer/R/"))
+
+library(ontologyIndex)
+library(jsonlite)
+library(ontologySimilarity)
+library(RCurl)
+
+data(go)
+data(gene_GO_terms)
+data(GO_IC)
+
+gene_json_data <- jsonlite::fromJSON('data/lm_plus_ar_genes.json')
+
+gene_list <- gene_json_data["gene_symbol"][[1]]
+
+beach <- gene_GO_terms[gene_list]
+
+#Go term selection top-5 based on information content (IC)
+go_terms <- c()
+for (i in (1:length(gene_list) )){
+  ic<-get_term_info_content(go,beach[[i]])[0:5]
+  go_selected <- beach[[i]][order(ic,decreasing = TRUE)]
+  #print(c(i,gene_list[[i]], go_selected))
+  go_terms <- c(go_terms, go_selected)
+}
+go_terms_unique <- sort(unique(go_terms))
+
+#Descriptors binary occurency vector
+
+gene_go_fingerprint <- matrix(0,length(gene_list),length(go_terms_unique))
+for (i in (1:length(go_terms_unique))){
+  for (j in (1:length(gene_list))){
+    if (length(which(beach[[j]] == go_terms_unique[[i]])) == 1){
+      gene_go_fingerprint[j,i] <- 1
+      
+    }
+  }
+}
+
+#Perform PCA analysis
+# pca <- prcomp(gene_go_fingerprint,scale = T)
+
+##Compute the explained variance for different number of principal components
+# pc <- cumsum(pca$sdev^2 / sum(pca$sdev^2))
+# gene_descriptors_pca <- pca$x[,0:which(pc>0.999)[1]]
+
+# rownames(gene_descriptors_pca) <- gene_list
+# colnames(gene_descriptors_pca) <- seq(1,ncol(gene_descriptors_pca))
+# write.csv(gene_descriptors_pca,file="data/cell_diff_gene_go_fingerprint_pca.csv", quote=FALSE)
+
+rownames(gene_go_fingerprint) <- gene_list
+colnames(gene_go_fingerprint) <- go_terms_unique
+write.csv(gene_go_fingerprint,file="data/lm_ar_gene_go_fingerprint.csv", quote=FALSE)
