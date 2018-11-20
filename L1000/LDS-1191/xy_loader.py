285b755cfb7e94583643b823114e36de4f8c31fd
gwoo <godwinwoo@hotmail.com>
2018-11-20 12:41:36 -0800

    Training

--- L1000/LDS-1191/xy_loader.py
+++ L1000/LDS-1191/xy_loader.py
@@ -3,7 +3,7 @@ from ensemble_optimizer_lite import do_optimize
 import numpy as np
 import random
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 import helpers.email_notifier as en
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
@@ -14,10 +14,10 @@ set_session(tf.Session(config=config))
 # target_cell_names = ['PC3', 'HT29']
 # target_cell_names = ['MCF7', 'A375']
 # target_cell_names = ['VCAP', 'A549']
-target_cell_names = ['MCF7']
+target_cell_names = ['VCAP']
 direction = 'Multi' #'Down'
-load_data_folder_path = "/data/datasets/gwoo/L1000/LDS-1191/ensemble_models/load_data/morgan2048/cell_diff/"
-data_folder_path = "/data/datasets/gwoo/L1000/LDS-1191/ensemble_models/cv/morgan2048/cell_diff/"
+load_data_folder_path = "/data/datasets/gwoo/L1000/LDS-1191/ensemble_models/load_data/morgan2048/ar/"
+data_folder_path = "/data/datasets/gwoo/L1000/LDS-1191/ensemble_models/cv/morgan2048/ar/"
 gap_factors = [0.0]
 percentiles = [5]
 class_weights = [0.01]
@@ -27,7 +27,7 @@ for target_cell_name in target_cell_names:
             for gap_factor in gap_factors:
                 for class_0_weight in class_weights:
                     file_suffix = target_cell_name + '_' + direction + '_' + str(bin) + 'b_' + \
-                                  str(percentile_down) + 'p_' + str(int(gap_factor*100)) + 'g_all35blind'
+                                  str(percentile_down) + 'p_' + str(int(gap_factor*100)) + 'g_all35Blind'
                     model_file_prefix = data_folder_path + str(datetime.datetime.now()) + '_' + file_suffix + \
                                         '_' + str(int(class_0_weight*100)) + 'c'
                     print('load location', load_data_folder_path)
