01d0d883184ef8f5d413804a79461e4388936cb5
gwoo <godwinwoo@hotmail.com>
2018-11-26 16:56:26 -0800

    Remove kekulize when calculated morgans. Zinc uses non kekulized.

--- L1000/LDS-1191/rdkit_fingerprint.py
+++ L1000/LDS-1191/rdkit_fingerprint.py
@@ -1,3 +1,5 @@
+# run this file locally with anaconda with rdkit and also openeye installed
+
 import os
 from rdkit.Chem import Descriptors, MACCSkeys
 import numpy as np
@@ -14,6 +16,19 @@ def load_csv(file):
             expression.append(row)
     return expression
 
+def get_feature_dict(file, delimiter=',', key_index=0, use_int=False):
+    with open(file, "r") as csv_file:
+        reader = csv.reader(csv_file, dialect='excel', delimiter=delimiter)
+        next(reader)
+        if use_int:
+            my_dict = {}
+            for row in reader:
+                list = []
+                for value in row[1:]:
+                    list.append(int(value))
+                my_dict[row[key_index]] = list
+            return my_dict
+        return dict((row[key_index], row[1:]) for row in reader)
 i=0
 finger_dimension = 2048
 molecules = []
@@ -25,13 +40,23 @@ names = []
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 print(path)
-# drug_dict = get_feature_dict('G:/GodwinWoo/LINCS/LDS-1191/Metadata/GSE92742_Broad_LINCS_pert_info.txt',
-#                              delimiter='\t', use_int=False)
+drug_dict = get_feature_dict('G:/GodwinWoo/LINCS/LDS-1191/Metadata/GSE92742_Broad_LINCS_pert_info.txt',
+                             delimiter='\t', use_int=False)
 # drug_dict = get_feature_dict('/data/datasets/gwoo/L1000/LDS-1191/Metadata/GSE92742_Broad_LINCS_pert_info.txt',
 #                              delimiter='\t', use_int=False)
-drug_dict = {}
-drug_dict['ZINC335870'] = ['','','','','','O[C@H](CCl)CNc1cccc2c(NC[C@@H](O)CCl)cccc12']
-drug_dict['ZINC145981020'] = ['','','','','','O[C@@H](CCl)COc1cccc2c(OC[C@@H](O)CCl)cccc12']
+# drug_dict = {}
+# drug_dict['EnzoLincs'] = ['','','','','','CNC(=O)c1ccc(cc1F)N1C(=S)N(C(=O)C1(C)C)c1ccc(C#N)c(c1)C(F)(F)F']
+# drug_dict['EnzaPubChem'] = ['','','','','','CC1(C(=O)N(C(=S)N1C2=CC(=C(C=C2)C(=O)NC)F)C3=CC(=C(C=C3)C#N)C(F)(F)F)C']
+# drug_dict['EnzaCanon'] = ['','','','','','CNC(=O)C1=C(F)C=C(C=C1)N2C(=S)N(C(=O)C2(C)C)C3=CC(=C(C=C3)C#N)C(F)(F)F']
+# drug_dict['Buclutomide'] = ['','','','','','CC(O)(C[S](=O)(=O)C1=CC=C(F)C=C1)C(=O)NC2=CC(=C(C=C2)C#N)C(F)(F)F']
+# drug_dict['Apolutamide'] = ['','','','','','CNC(=O)C1=C(F)C=C(C=C1)N2C(=S)N(C(=O)C23CCC3)C4=CN=C(C#N)C(=C4)C(F)(F)F']
+# drug_dict['Darolutamide'] = ['','','','','','CC(O)C1=CC(=N[NH]1)C(=O)NC(C)C[N]2C=CC(=N2)C3=CC(=C(C=C3)C#N)Cl']
+# drug_dict['VPC13789'] = ['','','','','','CC(C)NC(=O)C1=CC=CC2=NC(=CC=C12)C3=C[NH]C4=C(F)C(=C(F)C=C34)F']
+# drug_dict['VPC14449'] = ['','','','','','BrC1=C(Br)[N](C=N1)C2=CSC(=N2)N3CCOCC3']
+# drug_dict['VPC17005'] = ['','','','','','O=C(NC1=NCCS1)C2=CSC3=CC=CC=C23']
+# drug_dict['VPC220062'] = ['','','','','','OC(CCl)COC1=CC=C(Br)C2=C(OCC(O)CCl)C=CC=C12']
+# drug_dict['ZINC335870/VPC2055'] = ['','','','','','O[C@H](CCl)CNC1=C2C=CC=C(NC[C@@H](O)CCl)C2=CC=C1']
+# drug_dict['ZINC145981020/VPC220010'] = ['','','','','','O[C@@H](CCl)COC1=C2C=CC=C(OC[C@@H](O)CCl)C2=CC=C1']
 
 count = 0
 for key in drug_dict:
@@ -39,7 +64,7 @@ for key in drug_dict:
     try:
         smiles = drug_dict[key][5]
         m = Chem.MolFromSmiles(smiles)
-        Chem.Kekulize(m)
+        # Chem.Kekulize(m) # this generates different fps from cannonical smiles
         molecules.append(m)
         # fps.append(np.array([fp for fp in MACCSkeys.GenMACCSKeys(m).ToBitString()]))
         fp = np.zeros((1,))
@@ -61,4 +86,4 @@ id = id.reshape(len(fps),1)
 data = np.hstack((id,fps))
 header = np.array(header).reshape(1,len(header))
 data_header = np.vstack((header,data))
-np.savetxt("vpc_compounds_morgan_2048.csv", data_header, delimiter=",", fmt="%s")
+np.savetxt("vpc_compounds_morgan_2048_nk.csv", data_header, delimiter=",", fmt="%s")
